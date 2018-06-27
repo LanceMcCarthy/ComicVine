61a215fe81f6c73cbdd53379d06aff6c45dc8c62
Lance McCarthy <lance.mccarthy@telerik.com>
2018-06-27 13:10:20 -0400

    Added CustomListViewTemplateCell for CharactersPage

---
+++ ComicVine/ComicVine/Portable/CustomCells/CustomListViewTemplateCell.cs
@@ -0,0 +1,34 @@
+﻿using ComicVine.Portable.Models;
+using FFImageLoading.Forms;
+using Xamarin.Forms;
+
+namespace ComicVine.Portable.CustomCells
+{
+    public class CustomListViewTemplateCell : Telerik.XamarinForms.DataControls.ListView.ListViewTemplateCell
+    {
+        private CachedImage cachedImage;
+        private Label nameLabel;
+        private Label descriptionLabel;
+
+        public CustomListViewTemplateCell()
+        {
+            
+        }
+
+        protected override void OnBindingContextChanged()
+        {
+            if (BindingContext is Character character)
+            {
+                cachedImage = View.FindByName<CachedImage>("ThumbnailCachedImage");
+                nameLabel = View.FindByName<Label>("NameLabel");
+                descriptionLabel = View.FindByName<Label>("DecriptionLabel");
+
+                cachedImage.Source = character.Image.ThumbUrl;
+                nameLabel.Text = character.Name;
+                descriptionLabel.Text = character.Deck;
+            }
+            
+            base.OnBindingContextChanged();
+        }
+    }
+}
